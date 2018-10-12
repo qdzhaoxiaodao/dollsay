c6aca49a44505436c657d445e442345ec42f4ae5
superGG1990 <1132468176@qq.com>
2018-10-12 09:17:25 +0800

    aa

--- src/components/mainFooter.vue
+++ src/components/mainFooter.vue
@@ -1,126 +1,204 @@
 <template>
-  <div class="footer">
-  	<h3>友情链接</h3>
-  	<div class="yqlj">
-  		<div v-for="(item,index) in lianjie" :key='index'>
-  			<a :href="item.href">
-  				<img :src="item.src" alt="" />
-  				<span>{{item.name}}</span>
-  			</a>
-  		</div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  		<div style="height: 0;padding: 0; margin: 0 10px;"></div>
-  	</div>
-  	<div class="wangzhan">
-  		<p>
-    		<span>前端,ui设计by</span>
-    		<span>-----前端工程师<a href="www.tuneng.xyz">赵小神</a></span>
-    	</p>
-    	<p>
-    		<span>后台技术支持</span>
-    		<span>-----PHP高级工程师 <a href="www.cube6.top">陈四伟4wBLOG</a></span>
-    	</p>
-    	<p>
-    		<span>本网站由</span>
-    		<span>-----学姐之家ceo<a href="www.cube6.top">张阳</a>独家赞助</span>
-    	</p>
-  	</div>
-  </div>
+	<div class="footer">
+		<h3>友情链接</h3>
+		<div class="yqlj">
+			<div v-for="(item,index) in lianjie" :key='index'>
+				<a :href="item.href">
+					<img :src="item.src" alt="" />
+					<span>{{item.name}}</span>
+				</a>
+			</div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+			<div style="height: 0;padding: 0; margin: 0 10px;"></div>
+		</div>
+		<div class="wangzhan">
+			<p>
+				<span>前端,ui设计by</span>
+				<span>-----前端工程师<a href="http://www.tuneng.xyz">赵小神</a></span>
+			</p>
+			<p>
+				<span>后台技术支持</span>
+				<span>-----PHP高级工程师 <a href="http://www.cube6.top">陈四伟4wBLOG</a></span>
+			</p>
+			<p>
+				<span>本网站由</span>
+				<span>-----学姐之家ceo<a href="http://www.cube6.top">张阳</a>独家赞助</span>
+			</p>
+			<p>
+				<span>备案号：</span>
+				<span><a href="http://www.miitbeian.gov.cn">豫ICP备17041677号</a></span>
+			</p>
+		</div>
+	</div>
 </template>
 
 <script>
-import MyAjax from "@/function/MyAjax.js"
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      lianjie:[
-      	{name:'玩偶说',src:'../../static/img/logo.png',href:'www.tuneng.xyz'},
-      	{name:'4wBLOG',src:'../../static/img/4wblog.png',href:'www.cube6.top'},
-      	{name:'百度一下',src:'https://www.baidu.com/img/baidu_jgylogo3.gif',href:'www.baidu.com'},
-      	{name:'玩偶说',src:'../../static/img/logo.png',href:'www.tuneng.xyz'},
-      	{name:'4wBLOG',src:'../../static/img/4wblog.png',href:'www.cube6.top'},
-      	{name:'百度一下',src:'https://www.baidu.com/img/baidu_jgylogo3.gif',href:'www.baidu.com'},
-      	{name:'玩偶说',src:'../../static/img/logo.png',href:'www.tuneng.xyz'},
-      	{name:'4wBLOG',src:'../../static/img/4wblog.png',href:'www.cube6.top'},
-      	{name:'百度一下',src:'https://www.baidu.com/img/baidu_jgylogo3.gif',href:'www.baidu.com'},
-      	{name:'玩偶说',src:'../../static/img/logo.png',href:'www.tuneng.xyz'},
-      	{name:'4wBLOG',src:'../../static/img/4wblog.png',href:'www.cube6.top'},
-      	{name:'百度一下',src:'https://www.baidu.com/img/baidu_jgylogo3.gif',href:'www.baidu.com'},
-      	{name:'玩偶说',src:'../../static/img/logo.png',href:'www.tuneng.xyz'},
-      	{name:'4wBLOG',src:'../../static/img/4wblog.png',href:'www.cube6.top'},
-      	{name:'百度一下',src:'https://www.baidu.com/img/baidu_jgylogo3.gif',href:'www.baidu.com'},
-      	{name:'玩偶说',src:'../../static/img/logo.png',href:'www.tuneng.xyz'},
-      	{name:'4wBLOG',src:'../../static/img/4wblog.png',href:'www.cube6.top'},
-      	{name:'百度一下',src:'https://www.baidu.com/img/baidu_jgylogo3.gif',href:'www.baidu.com'},
-      ]
-    }
-  }
-}
+	import MyAjax from "@/function/MyAjax.js"
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App',
+				lianjie: [{
+						name: '玩偶说',
+						src: '../../static/img/logo.png',
+						href: 'http://www.tuneng.xyz'
+					},
+					{
+						name: '4wBLOG',
+						src: '../../static/img/4wblog.png',
+						href: 'http://www.cube6.top'
+					},
+					{
+						name: '百度一下',
+						src: 'https://www.baidu.com/img/baidu_jgylogo3.gif',
+						href: 'http://www.baidu.com'
+					},
+					{
+						name: '玩偶说',
+						src: '../../static/img/logo.png',
+						href: 'http://www.tuneng.xyz'
+					},
+					{
+						name: '4wBLOG',
+						src: '../../static/img/4wblog.png',
+						href: 'http://www.cube6.top'
+					},
+					{
+						name: '百度一下',
+						src: 'https://www.baidu.com/img/baidu_jgylogo3.gif',
+						href: 'http://www.baidu.com'
+					},
+					{
+						name: '玩偶说',
+						src: '../../static/img/logo.png',
+						href: 'http://www.tuneng.xyz'
+					},
+					{
+						name: '4wBLOG',
+						src: '../../static/img/4wblog.png',
+						href: 'http://www.cube6.top'
+					},
+					{
+						name: '百度一下',
+						src: 'https://www.baidu.com/img/baidu_jgylogo3.gif',
+						href: 'http://www.baidu.com'
+					},
+					{
+						name: '玩偶说',
+						src: '../../static/img/logo.png',
+						href: 'http://www.tuneng.xyz'
+					},
+					{
+						name: '4wBLOG',
+						src: '../../static/img/4wblog.png',
+						href: 'http://www.cube6.top'
+					},
+					{
+						name: '百度一下',
+						src: 'https://www.baidu.com/img/baidu_jgylogo3.gif',
+						href: 'http://www.baidu.com'
+					},
+					{
+						name: '玩偶说',
+						src: '../../static/img/logo.png',
+						href: 'http://www.tuneng.xyz'
+					},
+					{
+						name: '4wBLOG',
+						src: '../../static/img/4wblog.png',
+						href: 'http://www.cube6.top'
+					},
+					{
+						name: '百度一下',
+						src: 'https://www.baidu.com/img/baidu_jgylogo3.gif',
+						href: 'http://www.baidu.com'
+					},
+					{
+						name: '玩偶说',
+						src: '../../static/img/logo.png',
+						href: 'http://www.tuneng.xyz'
+					},
+					{
+						name: '4wBLOG',
+						src: '../../static/img/4wblog.png',
+						href: 'http://www.cube6.top'
+					},
+					{
+						name: '百度一下',
+						src: 'https://www.baidu.com/img/baidu_jgylogo3.gif',
+						href: 'http://www.baidu.com'
+					},
+				]
+			}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-p{
-	color: #2c3e50;
-	a{
-		color: #f66;
-		font-size: 20px;
+	p {
+		color: #2c3e50;
+		a {
+			color: #f66;
+			font-size: 20px;
+		}
+		span:nth-of-type(1) {
+			font-size: 20px;
+		}
 	}
-	span:nth-of-type(1){
-		font-size: 20px;
-	}
-}
-h3{
-	text-align: center;
-	margin: 20px;
-}
-.yqlj{
-	width: 950px;
-	margin: 20px auto;
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-around;
-	div{
-		width: 120px;
-		height: 67px;
+	
+	h3 {
 		text-align: center;
-		background: #fff;
-		padding: 5px;
-		margin: 10px 10px;
-		border-radius: 6px;
-		a{
-			img{
+		margin: 20px;
+	}
+	
+	.yqlj {
+		width: 950px;
+		margin: 20px auto;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		div {
+			width: 120px;
+			height: 67px;
+			text-align: center;
+			background: #fff;
+			padding: 5px;
+			margin: 10px 10px;
+			border-radius: 6px;
+			a {
+				img {
 					width: 100px;
 					height: 35px;
 				}
-			span{
-				margin: 10px;
-				height: 20px;
-				line-height: 20px;
-				font-size: 16px;
-				color: #333;
+				span {
+					margin: 10px;
+					height: 20px;
+					line-height: 20px;
+					font-size: 16px;
+					color: #333;
+				}
 			}
 		}
 	}
-}
-.wangzhan{
-	width: 100%;
-	text-align: center;
-	display: flex;
-	justify-content:center;
-	flex-wrap: wrap;
-	p:nth-of-type(2){
-		margin: 0 10px;
+	
+	.wangzhan {
+		width: 100%;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+		p:nth-of-type(2) {
+			margin: 0 10px;
+		}
 	}
-}
 </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,319 +1,258 @@
 <template>
-  <div class="home">
-   <!--<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/dlb01.jpg" width="100%" alt="" />-->
-   <div class="home-main">
-   	<div class="main-top">
-   		<div class="main-top-top">
-   			<div class="main-top-top-left">
-   				<h3>本网站已收录：</h3>
-   				<p>
-   					<span>300</span>
-   					<span>篇玩偶文章</span>
-   				</p>
-   				<p>
-   					<span>180</span>
-   					<span>个玩偶系列</span>
-   				</p>
-   				<p>
-   					<span>2000000</span>
-   					<span>款玩偶</span>
-   				</p>
-   			</div>
-   			<div class="main-top-top-right">
-   				<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
-   				<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_03.jpg" alt="" />
-   				<div class="p-img">
-   					<p class="p-text" style="text-align: right;">
-                        一直在这里等你，不吵不闹。<br>
-                        橱窗里，淡淡的忧伤，孤单却又满怀希望。<br>
-                        我知道，很久很久以前的你，怕黑，怕一个人，但是，其实。<br>
-                        你知道吗，我一直在等你来。<br>
-                        带走属于你的我，陪你难过，大笑，即使我无法开口，但是。<br>
-                        	我懂你的忧伤，可是，最后你却没有带走我。<br>
-                        <br>
-                      </p>
-   				</div>
-   			</div>
-   		</div>
-   		<div class="main-top-bot">
+	<div class="home">
+		<!--<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/dlb01.jpg" width="100%" alt="" />-->
+		<div class="home-main">
+			<div class="main-top">
+				<div class="main-top-top">
+					<div class="main-top-top-left">
+						<h3>本网站已收录：</h3>
+						<p>
+							<span>300</span>
+							<span>篇玩偶文章</span>
+						</p>
+						<p>
+							<span>180</span>
+							<span>个玩偶系列</span>
+						</p>
+						<p>
+							<span>2000000</span>
+							<span>款玩偶</span>
+						</p>
+					</div>
+					<div class="main-top-top-right">
+						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
+						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_03.jpg" alt="" />
+						<div class="p-img">
+							<p class="p-text" style="text-align: right;">
+								一直在这里等你，不吵不闹。<br> 橱窗里，淡淡的忧伤，孤单却又满怀希望。
+								<br> 我知道，很久很久以前的你，怕黑，怕一个人，但是，其实。
+								<br> 你知道吗，我一直在等你来。
+								<br> 带走属于你的我，陪你难过，大笑，即使我无法开口，但是。
+								<br> 我懂你的忧伤，可是，最后你却没有带走我。
+								<br>
+								<br>
+							</p>
+						</div>
+					</div>
+				</div>
+				<!--<div class="main-top-bot">
    			<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_04.jpg" alt="" />
    			<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_05.jpg" alt="" />
-   		</div>
-   	</div>
-   </div>
-   	<div class="news">
-   			最新收录————>>
-   		</div>
-   		<div class="main-content">
-   			<div class="main-left">
-   				<div class="tablist">
-   					<div class="img">
-   						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
-   					</div>
-   					<div class="introduce">
-   						<p>第1234567期：《玩具熊的五夜后宫系列》</p>
-   						<p>标签：<span>新系列</span></p>
-   						<p>《玩具熊的五夜后宫 Five Nights at Freddy》玩具熊的五夜后宫是独立的点击式生存恐怖电视游戏和由Scott Cawthon开发的系列的第一部分。 它于2014年8月9日在Desura上首次发布，并于2014年8月18日在Steam上发布。玩家的目标是在Freddy's Fazbears Pizza披萨店店里生存五晚，同时避免被机械玩偶捕捉和杀死，并通过店铺的闭路监控系统来监控机械玩偶们。
+   		</div>-->
+			</div>
+		</div>
+		<div class="news">
 
-“白天，这是一个快乐的地方，但你白天不在这里。 你只有夜晚。”</p>
-   						<p>2018-12-12 12:24:00</p>
-   					</div>
-   				</div>
-   				<div class="tablist">
-   					<div class="img">
-   						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
-   					</div>
-   					<div class="introduce">
-   						<p>第1234567期：《玩具熊的五夜后宫系列》</p>
-   						<p>标签：<span>新系列</span></p>
-   						<p>《玩具熊的五夜后宫 Five Nights at Freddy》玩具熊的五夜后宫是独立的点击式生存恐怖电视游戏和由Scott Cawthon开发的系列的第一部分。 它于2014年8月9日在Desura上首次发布，并于2014年8月18日在Steam上发布。玩家的目标是在Freddy's Fazbears Pizza披萨店店里生存五晚，同时避免被机械玩偶捕捉和杀死，并通过店铺的闭路监控系统来监控机械玩偶们。
-
-“白天，这是一个快乐的地方，但你白天不在这里。 你只有夜晚。”</p>
-   						<p>2018-12-12 12:24:00</p>
-   					</div>
-   				</div>
-   				<div class="tablist">
-   					<div class="img">
-   						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
-   					</div>
-   					<div class="introduce">
-   						<p>第1234567期：《玩具熊的五夜后宫系列》</p>
-   						<p>标签：<span>新系列</span></p>
-   						<p>《玩具熊的五夜后宫 Five Nights at Freddy》玩具熊的五夜后宫是独立的点击式生存恐怖电视游戏和由Scott Cawthon开发的系列的第一部分。 它于2014年8月9日在Desura上首次发布，并于2014年8月18日在Steam上发布。玩家的目标是在Freddy's Fazbears Pizza披萨店店里生存五晚，同时避免被机械玩偶捕捉和杀死，并通过店铺的闭路监控系统来监控机械玩偶们。
-
-“白天，这是一个快乐的地方，但你白天不在这里。 你只有夜晚。”</p>
-   						<p>2018-12-12 12:24:00</p>
-   					</div>
-   				</div>
-   				<div class="tablist">
-   					<div class="img">
-   						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
-   					</div>
-   					<div class="introduce">
-   						<p>第1234567期：《玩具熊的五夜后宫系列》</p>
-   						<p>标签：<span>新系列</span></p>
-   						<p>《玩具熊的五夜后宫 Five Nights at Freddy》玩具熊的五夜后宫是独立的点击式生存恐怖电视游戏和由Scott Cawthon开发的系列的第一部分。 它于2014年8月9日在Desura上首次发布，并于2014年8月18日在Steam上发布。玩家的目标是在Freddy's Fazbears Pizza披萨店店里生存五晚，同时避免被机械玩偶捕捉和杀死，并通过店铺的闭路监控系统来监控机械玩偶们。
-
-“白天，这是一个快乐的地方，但你白天不在这里。 你只有夜晚。”</p>
-   						<p>2018-12-12 12:24:00</p>
-   					</div>
-   				</div>
-   				<div class="tablist">
-   					<div class="img">
-   						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
-   					</div>
-   					<div class="introduce">
-   						<p>第1234567期：《玩具熊的五夜后宫系列》</p>
-   						<p>标签：<span>新系列</span></p>
-   						<p>《玩具熊的五夜后宫 Five Nights at Freddy》玩具熊的五夜后宫是独立的点击式生存恐怖电视游戏和由Scott Cawthon开发的系列的第一部分。 它于2014年8月9日在Desura上首次发布，并于2014年8月18日在Steam上发布。玩家的目标是在Freddy's Fazbears Pizza披萨店店里生存五晚，同时避免被机械玩偶捕捉和杀死，并通过店铺的闭路监控系统来监控机械玩偶们。
-
-“白天，这是一个快乐的地方，但你白天不在这里。 你只有夜晚。”</p>
-   						<p>2018-12-12 12:24:00</p>
-   					</div>
-   				</div>
-   				<div class="tablist">
-   					<div class="img">
-   						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
-   					</div>
-   					<div class="introduce">
-   						<p>第1234567期：《玩具熊的五夜后宫系列》</p>
-   						<p>标签：<span>新系列</span></p>
-   						<p>《玩具熊的五夜后宫 Five Nights at Freddy》玩具熊的五夜后宫是独立的点击式生存恐怖电视游戏和由Scott Cawthon开发的系列的第一部分。 它于2014年8月9日在Desura上首次发布，并于2014年8月18日在Steam上发布。玩家的目标是在Freddy's Fazbears Pizza披萨店店里生存五晚，同时避免被机械玩偶捕捉和杀死，并通过店铺的闭路监控系统来监控机械玩偶们。
-
-“白天，这是一个快乐的地方，但你白天不在这里。 你只有夜晚。”</p>
-   						<p>2018-12-12 12:24:00</p>
-   					</div>
-   				</div>
-   				<router-link to='/more' tag='div' class="more">发现更多>>>></router-link>
-   			</div>
-   			<div class="main-right">
-   				<div style="width: 300px;">
-   					<a href="http://www.unicef.cn/cn/">
-   						<img src="https://tpc.googlesyndication.com/daca_images/simgad/12828719771019780790" border="0" width="300" alt="" class="img_ad">
-   					</a>
-   				</div>
-   			</div>
-   		</div>
-  </div>
+		</div>
+		<div class="main-content">
+			<div class="main-left">
+				<div class="tablist" v-for="item in 6">
+					<div class="img">
+						<img src="http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_02.jpg" alt="" />
+					</div>
+					<div class="introduce">
+						<p>第1234567期：《玩具熊的五夜后宫系列》</p>
+						<p>《玩具熊的五夜后宫 Five Nights at Freddy》玩具熊的五夜后宫是独立的点击式生存恐怖电视游戏和由Scott Cawthon开发的系列的第一部分。 它于2014年8月9日在Desura上首次发布，并于2014年8月18日在Steam上发布。玩家的目标是在Freddy's Fazbears Pizza披萨店店里生存五晚，同时避免被机械玩偶捕捉和杀死，并通过店铺的闭路监控系统来监控机械玩偶们。 “白天，这是一个快乐的地方，但你白天不在这里。 你只有夜晚。”</p>
+					</div>
+					<div class="xilie">
+						<p>标签：<span>新系列</span></p>
+						<p>2018-12-12 12:24:00</p>
+					</div>
+				</div>
+				<router-link to='/more' tag='div' class="more">发现更多>>>></router-link>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import MyAjax from "@/function/MyAjax.js"
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
+	import MyAjax from "@/function/MyAjax.js"
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		},
 		mounted() {
 			window.scrollTo(0, 0);
 		},
-}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.home{
-	width: 100%;
-	margin-top: 145px;
-	overflow: hidden;
-	.home-main{
-		width: 950px;
-		margin: 0 auto;
-		.main-top{
-			width: 100%;
-			height: 712px;
-			.main-top-top{
+	.home {
+		width: 100%;
+		margin-top: 110px;
+		overflow: hidden;
+		.home-main {
+			width: 950px;
+			margin: 0 auto;
+			.main-top {
 				width: 100%;
 				height: 402px;
-				margin-bottom:13px;
-				.main-top-top-left{
-					width: 402px;
+				.main-top-top {
+					width: 100%;
 					height: 402px;
-					float: left;
-					background: url('http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_01.jpg') no-repeat;
-					background-size: 402px 402px;
-					color: #f4fff1;
-					h3{
-						margin: 40px 0 20px;
-						padding-left: 30px;
-					}
-					p{
-						text-align: center;
-						width: 100%;
-						margin-bottom: 20px;
-						span{
-							display: inline;
-						}
-						span:nth-of-type(1){
-							color: #46b6cf;
-							font-size: 40px;
-						}
-						span:nth-of-type(2){
-							font-size: 20px;
-						}
-					}
-				}
-				.main-top-top-right{
-					width: 535px;
-					height: 402px;
-					float: right;
-					img:nth-of-type(1){
+					margin-bottom: 13px;
+					.main-top-top-left {
+						width: 402px;
+						height: 402px;
 						float: left;
+						background: url('http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/f519_01.jpg') no-repeat;
+						background-size: 402px 402px;
+						color: #f4fff1;
+						h3 {
+							margin: 40px 0 20px;
+							padding-left: 30px;
+						}
+						p {
+							text-align: center;
+							width: 100%;
+							margin-bottom: 20px;
+							span {
+								display: inline;
+							}
+							span:nth-of-type(1) {
+								color: #46b6cf;
+								font-size: 40px;
+							}
+							span:nth-of-type(2) {
+								font-size: 20px;
+							}
+						}
 					}
-					img:nth-of-type(2){
+					.main-top-top-right {
+						width: 535px;
+						height: 402px;
 						float: right;
-						margin-bottom: 3px;
+						img:nth-of-type(1) {
+							float: left;
+						}
+						img:nth-of-type(2) {
+							float: right;
+							margin-bottom: 3px;
+						}
+						.p-img {
+							float: left;
+							width: 100%;
+							height: 126px;
+							background: #f4fff1;
+						}
+						.p-text {
+							color: #01A3EE;
+							height: 126px;
+							line-height: 20px;
+							width: 537px;
+							font-size: 12px;
+							padding: 5px;
+							margin: 0px;
+							text-decoration: none;
+							background: url('http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/F419_bg.jpg') left top no-repeat;
+						}
 					}
-					.p-img{
+				}
+				.main-top-bot {
+					height: 287px;
+					width: 950px;
+					img:nth-of-type(1) {
 						float: left;
-						width: 100%;
-						height: 126px;
-						background: #f4fff1;
 					}
-					.p-text{
-						  color: #01A3EE;
-						  height: 126px;
-					    line-height: 20px;
-					    width: 537px;
-					    font-size: 12px;
-					    padding: 5px;
-					    margin: 0px;
-					    text-decoration: none;
-					    background: url('http://wanoushuo.oss-cn-beijing.aliyuncs.com/static/F419_bg.jpg') left top no-repeat;
+					img:nth-of-type(2) {
+						float: right;
 					}
 				}
 			}
-			.main-top-bot{
-				height:287px;
-				width: 950px;
-				img:nth-of-type(1){
-					float: left;
-				}
-				img:nth-of-type(2){
-					float: right;
-				}
-			}
-			
 		}
-	}
-	.news{
+		.news {
 			width: 100%;
 			min-height: 50px;
 			line-height: 50px;
 			text-align: center;
 			color: #fd9eb2;
-			font-size:25px;
+			font-size: 25px;
+			margin: 33px 0;
+			background: url(../../static/img/news.png) no-repeat center center;
 		}
-		.main-content{
+		.main-content {
 			width: 950px;
 			margin: 0 auto;
-			.main-left{
-				width: 630px;
+			.main-left {
+				width: 100%;
 				min-height: 600px;
 				float: left;
-				.tablist{
+				.tablist {
 					width: 100%;
 					height: 200px;
 					background: #fff;
 					padding: 10px;
 					color: #5c686d;
-					box-shadow: 0 0 5px #f66;
+					box-shadow: 1px 2px 6px #f39a9a;
 					margin-bottom: 20px;
-					.img{
+					.img {
 						width: 190px;
 						height: 190px;
 						float: left;
-						img{
+						img {
 							width: 180px;
 							height: 180px;
 						}
 					}
-					.introduce{
-						float: right;
+					.introduce {
+						float: left;
 						width: 400px;
 						height: 190px;
-						p:nth-of-type(1){
+						margin-left: 90px;
+						p:nth-of-type(1) {
 							font-size: 18px;
-							color: #2085a7;
-							margin-bottom: 5px;
-						}
-						p:nth-of-type(2){
-							font-size: 16px;
-							color: #000;
-							margin-bottom: 5px;
+							color: #1684a9;
+							margin-top: 20px;
+							margin-bottom: 30px;
 							text-align: right;
-							span{
-								color: #42B983;
-								margin-right: 12px;
-							}
 						}
-						p:nth-of-type(3){
+						p:nth-of-type(2) {
 							height: 90px;
 							line-height: 30px;
 							font-size: 16px;
 							color: #6d696a;
 							margin-bottom: 5px;
 							text-indent: 24px;
-						overflow: hidden;
-						text-overflow: ellipsis;
-						display:-webkit-box; 
-						-webkit-box-orient:vertical; 
-						-webkit-line-clamp:3;
-						cursor: pointer;
+							overflow: hidden;
+							text-overflow: ellipsis;
+							display: -webkit-box;
+							-webkit-box-orient: vertical;
+							-webkit-line-clamp: 3;
+							cursor: pointer;
 						}
-						p:nth-of-type(4){
+					}
+					.xilie {
+						width: 225px;
+						height: 190px;
+						float: right;
+						p:nth-of-type(1) {
+							font-size: 16px;
+							color: #000;
+							text-align: left;
+							margin-top:77px;
+							padding-left:30px;
+							margin-bottom:38px;
+							span {
+								color: #42B983;
+								margin-right: 12px;
+							}
+						}
+						p:nth-of-type(2) {
 							font-size: 18px;
-							text-align: right;
+							text-align: left;
 							padding-right: 12px;
 						}
 					}
 				}
-				.tablist:hover{
-					box-shadow: 0 0 5px #409eff;
+				.tablist:hover {
+					box-shadow: 1px 2px 6px #409eff;
 				}
-				.more{
+				.more {
 					width: 200px;
 					margin: 30px auto;
 					text-align: center;
@@ -322,10 +261,6 @@ export default {
 					color: #01A3EE;
 				}
 			}
-			.main-right{
-				width: 300px;
-				float: right;
-			}
 		}
-}
+	}
 </style>
